69989f9f58cb9fd974554073cd319c9c8b3fc8ed
Erickkirui <erickkirui653@gmail.com>
2023-03-16 15:18:50 +0300

    creteated an array with the two functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Code your solution in this file!
+//return first two drivers
 const returnFirstTwoDrivers = function (drivers){ 
     return drivers.slice(0, 2);
     
@@ -9,10 +10,16 @@ const drivers = ['erick' , 'koech' , 'kirui']
 const firstTwodrivers = returnFirstTwoDrivers(drivers)
 console.log (firstTwodrivers)
 
+// return last two drivers
+
 const returnLastTwoDrivers = function (lastDrivers){
  return lastDrivers.slice(-2);
 
 }
 const lastDrivers = ["erick" , "kirui","koech"]
-const lastTwoDrivers = returnLastTwoDrivers();
+const lastTwoDrivers = returnLastTwoDrivers(lastDrivers);
 console.log(lastDrivers)
+
+// function containing the above two functions
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
