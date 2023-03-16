da2f0c5de83efa62817e05cf3fbf970d2c82334a
Erickkirui <erickkirui653@gmail.com>
2023-03-16 14:35:58 +0300

    return first two and last two drivers

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers){ 
+    return drivers.slice(0, 2);
+    
+}
+
+const drivers = ['erick' , 'koech' , 'kirui']
+
+const firstTwodrivers = returnFirstTwoDrivers(drivers)
+console.log (firstTwodrivers)
+
+const returnLastTwoDrivers = function (lastDrivers){
+ return lastDrivers.slice(-2);
+
+}
+const lastDrivers = ["erick" , "kirui","koech"]
+const lastTwoDrivers = returnLastTwoDrivers();
+console.log(lastDrivers)
